c9b9ee9d3ac2184fd1d74dabc1eb3deaccff8278
Milena Neves Domingues <milena_neves95@hotmail.com>
2022-11-15 16:52:05 -0300

    uhuuuuuuuuuuuuuuul

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,21 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # verb 'path', to: 'controller#action'
+
+  # READ all restaurants
+  get "tasks", to: "tasks#index"
+
+  # CREATE a restaurant
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+
+  # READ one restaurant
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE a restaurant
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task # GET the HTML form (pre-filled with restaurant attributes) for editing = 1 request
+  patch 'tasks/:id', to: 'tasks#update' # PATCH the parameters to update an existing restaurant = 1 request
+
+  delete 'tasks/:id', to: 'tasks#destroy', as: :destroy_task
 end
